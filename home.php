4095ab419b8559e3678f41a1c51e7c2e48ffad84
Ibnu Putra Ardiansyah <azxarblog@gmail.com>
2018-10-19 11:26:29 +0700

    coonect to php

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "Ibnu Putra Ardiansyah"; #variabel nama
+define(NILAI, 100);
+echo "Halo temen - temen , Nama Saya "; /*output yang akan
+ditambah dengan variabel nama*/
+echo $nama; //tampilkan isi variabel nama
+echo "<br> Nilai : " . NILAI;
+
+?>
